ecca86a043e0b92e70e1a1663181bf727341ebc9
nicolas21467 <81382503+nicolas21467@users.noreply.github.com>
2021-09-20 14:18:22 -0300

    aa

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import  {RegistroService} from '../registro.service';
 
 @Component({
   selector: 'app-registro',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.page.scss'],
 })
 export class RegistroPage implements OnInit {
+  user = []
+  constructor(private registroservice:RegistroService) { }
 
-  constructor() { }
 
   ngOnInit() {
+    // traspasar los datos del servicio al atributo  
+    this.user = this.registroservice.getUsuarios();
   }
-
+  ionViewWillEnter()
+  {
+    this.user = this.registroservice.getUsuarios();
+  }
 }
